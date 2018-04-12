3f3cf85a7da553bf5f6340a6a572ad16f754baa4
KapiN <36258556+nandi323@users.noreply.github.com>
2018-04-12 22:44:00 +0300

    Nothing to say

--- DofusMovement/learnToDeacode.py
+++ DofusMovement/learnToDeacode.py
@@ -5,6 +5,7 @@ import os
 from PIL import Image
 import time
 from sklearn import svm
+from sklearn import datasets
 
 src_path = 'C:/Users/Kapi/Documents/GitHub/ezNemEgyRobotMondomNemAz/DofusMovement/analyze/noisefree/test/'
 
@@ -24,37 +25,52 @@ for file in os.listdir(src_path):
 
 numpyArray = np.array(dataset)
 
-with open('imagesInArrayFormat.txt', 'w') as outfile:
-    # I'm writing a header here just for the sake of readability
-    # Any line starting with "#" will be ignored by numpy.loadtxt
-    # outfile.write('# Array shape: {0}\n'.format(numpyArray.shape))
+# with open('imagesInArrayFormat.txt', 'w') as outfile:
+#     # I'm writing a header here just for the sake of readability
+#     # Any line starting with "#" will be ignored by numpy.loadtxt
+#     # outfile.write('# Array shape: {0}\n'.format(numpyArray.shape))
+#
+#     # Iterating through a ndimensional array produces slices along
+#     # the last axis. This is equivalent to data[i,:,:] in this case
+#     for data_slice in numpyArray:
+#
+#         # The formatting string indicates that I'm writing out
+#         # the values in left-justified columns 7 characters in width
+#         # with 2 decimal places.
+#         np.savetxt(outfile, data_slice)
+#
+#         # Writing out a break to indicate different slices...
+#         # outfile.write('# New slice\n')
+# outfile.close()
 
-    # Iterating through a ndimensional array produces slices along
-    # the last axis. This is equivalent to data[i,:,:] in this case
-    for data_slice in numpyArray:
 
-        # The formatting string indicates that I'm writing out
-        # the values in left-justified columns 7 characters in width
-        # with 2 decimal places.
-        np.savetxt(outfile, data_slice)
-
-        # Writing out a break to indicate different slices...
-        # outfile.write('# New slice\n')
-outfile.close()
-
-
-newData = np.loadtxt('imagesInArrayFormat.txt')
-alma = np.array(newData.reshape(len(dataset),16,90))
+# newData = np.loadtxt('imagesInArrayFormat.txt')
+# alma = np.array(newData.reshape(len(dataset),16,90))
 
 # plt.imshow(alma[1])
 # plt.show()
+#
+# for data in dataset:
+#     plt.imshow(data)
+#     plt.show()
+#     szam = input("add meg a kepen lathato szamot : ")
+#     resultset.append(szam)
+#     plt.close()
+#
+# np.save('resultDataset', resultset)
 
-for data in dataset:
-    plt.imshow(data)
-    plt.show()
-    szam = input("add meg a kepen lathato szamot : ")
-    resultset.append(szam)
-    plt.close()
+np.set_printoptions(threshold=np.nan)
+clf = svm.SVC(gamma=0.001, C=100)
+x = np.loadtxt('imagesInArrayFormat.txt')
+alma = np.array(x.reshape(len(dataset),16,90))
+digit = datasets.load_digits()
 
-np.save('resultDataset', resultset)
+flatim = alma.flatten()
+
+X = alma.reshape(14,1440)
+y = np.load("resultDataset.npy")
+
+clf.fit(X,y)
+
+print('predict', clf.predict(X))
 
--- DofusMovement/test.py
+++ DofusMovement/test.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
 import numpy as np
 
-alma = np.load("resultDataset.txt.npy")
+src_path = 'C:/Users/Kapi/Documents/GitHub/ezNemEgyRobotMondomNemAz/DofusMovement/analyze/noisefree/test/'
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
-print(alma)
+print(pytesseract.image_to_string(Image.open('analyze/noisefree/test/alma.jpg')))
